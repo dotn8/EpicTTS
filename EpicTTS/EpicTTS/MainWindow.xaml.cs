130f424d50eeebccd0946263774653115fdf070e
dotn8 <dotn8@users.noreply.github.com>
2014-05-30 21:00:35 -0500

    Add a voice selector

--- EpicTTS/EpicTTS/MainWindow.xaml.cs
+++ EpicTTS/EpicTTS/MainWindow.xaml.cs
@@ -1,28 +1,15 @@
-﻿using FirstFloor.ModernUI.Windows.Controls;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using EpicTTS.Models;
 
 namespace EpicTTS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow
     {
         public MainWindow()
         {
+            DataContext = new MainModel();
             InitializeComponent();
         }
     }
---
+++ EpicTTS/EpicTTS/Models/MainModel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Speech.Synthesis;
+
+namespace EpicTTS.Models
+{
+    public class MainModel
+    {
+        private readonly SpeechSynthesizer _synthesizer;
+        public IList<InstalledVoice> Voices { get; private set; }
+
+        public MainModel()
+        {
+            _synthesizer = new SpeechSynthesizer();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            InitializeVoices();
+        }
+
+        private void InitializeVoices()
+        {
+            Voices = new List<InstalledVoice>(_synthesizer.GetInstalledVoices());
+        }
+    }
+}
