2f35d58cd1ea7d6b0edcc021cd0f4b9c9a8f2ab9
dotn8 <dotn8@users.noreply.github.com>
2014-05-31 07:19:34 -0500

    Add speak, stop, and pause commands and buttons

---
+++ EpicTTS/EpicTTS/Converters/DictionaryConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace EpicTTS.Converters
+{
+    public class DictionaryConverter : Dictionary<object, object>, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            object result;
+            if (TryGetValue(value, out result))
+                return result;
+            throw new Exception("Unknown key " + value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return this.First(kvp => kvp.Value == value).Key;
+        }
+    }
+}
--- EpicTTS/EpicTTS/Models/MainModel.cs
+++ EpicTTS/EpicTTS/Models/MainModel.cs
@@ -1,9 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
-using System.Speech.AudioFormat;
 using System.Speech.Synthesis;
+using System.Windows.Input;
 using EpicTTS.Annotations;
+using FirstFloor.ModernUI.Presentation;
 
 namespace EpicTTS.Models
 {
@@ -13,11 +14,42 @@ namespace EpicTTS.Models
         private InstalledVoice _selectedVoice;
         private string _text;
         public IList<InstalledVoice> Voices { get; private set; }
+        public ICommand SpeakCommand { get; private set; }
+        public ICommand StopSpeakingCommand { get; private set; }
+        public ICommand PauseSpeakingCommand { get; private set; }
+        public SynthesizerState State { get { return _synthesizer.State; } }
 
         public MainModel()
         {
             _synthesizer = new SpeechSynthesizer();
             Initialize();
+            SpeakCommand = new RelayCommand(Speak);
+            StopSpeakingCommand = new RelayCommand(StopSpeaking);
+            PauseSpeakingCommand = new RelayCommand(PauseSpeaking);
+            _synthesizer.StateChanged += StateChanged;
+        }
+
+        private void PauseSpeaking(object obj)
+        {
+            _synthesizer.Pause();
+        }
+
+        private void StateChanged(object sender, StateChangedEventArgs e)
+        {
+            OnPropertyChanged("State");
+        }
+
+        private void Speak(object obj)
+        {
+            if (_synthesizer.State == SynthesizerState.Paused)
+                _synthesizer.Resume();
+            else
+                _synthesizer.SpeakAsync(Text);
+        }
+
+        private void StopSpeaking(object obj)
+        {
+            _synthesizer.SpeakAsyncCancelAll();
         }
 
         private void Initialize()
@@ -32,6 +64,7 @@ namespace EpicTTS.Models
             {
                 if (Equals(value, _selectedVoice)) return;
                 _selectedVoice = value;
+                _synthesizer.SelectVoice(_selectedVoice.VoiceInfo.Name);
                 OnPropertyChanged();
             }
         }
