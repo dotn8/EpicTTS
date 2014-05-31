a435ef822ca03af63d7508ad8b19c816f4bb3550
dotn8 <dotn8@users.noreply.github.com>
2014-05-30 20:37:01 -0500

    Add the fluent ribbon control

--- EpicTTS/EpicTTS/MainWindow.xaml.cs
+++ EpicTTS/EpicTTS/MainWindow.xaml.cs
@@ -19,7 +19,7 @@ namespace EpicTTS
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : ModernWindow
+    public partial class MainWindow
     {
         public MainWindow()
         {
