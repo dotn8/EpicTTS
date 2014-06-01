aa7fa55c55c0c4ff91ca30d48c0003678719fe66
dotn8 <dotn8@users.noreply.github.com>
2014-06-01 07:45:44 -0500

    Add the ability to browse and open text file from GUI

--- EpicTTS/EpicTTS/Models/TextDocument.cs
+++ EpicTTS/EpicTTS/Models/TextDocument.cs
@@ -1,25 +1,54 @@
+using System;
 using System.IO;
 using System.Speech.Synthesis;
+using System.Windows.Input;
+using System.Windows.Interop;
+using FirstFloor.ModernUI.Presentation;
 
 namespace EpicTTS.Models
 {
-    public class TextDocument
+    public class TextDocument : ObservableObject
     {
-        public string Text { get; set; }
-        public string FilePath { get; set; }
+        private string _filePath;
+        private string _text;
+        public ICommand BrowseCommand { get; private set; }
+
+        public string Text
+        {
+            get { return GetProperty(ref _text); }
+            set { SetProperty(out _text, value); }
+        }
+
+        public string FilePath
+        {
+            get { return GetProperty(ref _filePath); }
+            set { SetProperty(out _filePath, value); }
+        }
 
         public TextDocument()
         {
             Text = "";
+            BrowseCommand = new RelayCommand(obj => Browse());
+        }
+
+        private void Browse()
+        {
+            var dlg = new Microsoft.Win32.OpenFileDialog {DefaultExt = ".txt", Filter = "Text documents (.txt)|*.txt"};
+            var result = dlg.ShowDialog();
+            if (result == true)
+            {
+                FilePath = dlg.FileName;
+                Open();
+            }
         }
 
         public void Open(string path)
         {
             FilePath = path;
-            Reload();
+            Open();
         }
 
-        public void Reload()
+        public void Open()
         {
             Text = File.ReadAllText(FilePath);
         }
